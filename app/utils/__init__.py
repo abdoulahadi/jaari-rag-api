23621678fa2df25d8e7c83cae7317f2f8e8bec30
CHEIKH ABDOUL AHAD MBACKE DIOP <caambdiop@CHEIKHs-MacBook-Pro.local>
2025-09-25 19:08:13 +0000

    fix: Export setup_google_credentials function in utils/__init__.py
    - Add proper imports in app/utils/__init__.py
    - Export setup_google_credentials and get_google_credentials_path
    - This fixes the import error preventing Wolof translation from working

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,7 @@
 """
 Utilities package for Jaari RAG API
 """
+
+from .google_credentials import setup_google_credentials, get_google_credentials_path
+
+__all__ = ['setup_google_credentials', 'get_google_credentials_path']
